92c6805ae7f8ddbaf716bf38aaf80b39773ad4a9
Muzzamil hassan <129590453+muzzamilhassan@users.noreply.github.com>
2024-08-06 12:07:18 +0500

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <Navbar/>
         </div>
     
-        <img src="public\images\hero_1.jpg.webp" alt=" logo" className='absolute bg-red-500 top-0 left-0 -z-10 w-full h-screen'/>
+        <img src="\public\images\hero_1.jpg.webp" alt=" logo" className='absolute bg-red-500 top-0 left-0 -z-10 w-full h-screen'/>
  
             <span className='w-full h-screen absolute top-0 left-0 bg-black opacity-20 -z-10'></span>
         <div className='flex flex-col justify-center items-center space-y-4 mt-20'>
